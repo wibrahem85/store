2215a2e57e6af09592420703ba3351c09ecc1418
wibrahem85 <wibrahem85@gmail.com>
2019-03-26 05:49:42 +0200

    store templete
    the site is responsive and compatible with IE.
    used techniques:
    html,  html5, css, css3, bootstrap4, javascript,  jquery.

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	
+	$(".h6-1").click(function(){
+		
+		$('.ul-1').slideToggle('slow');
+		$(".h6-1").toggleClass('change-ul-color');
+		$(".h6-1 span").text("-");	
+			
+		
+			
+		
+				
+
+	});
+	
+	$(".h6-2").click(function(){
+		
+		$('.ul-2').slideToggle('slow');
+		$(".h6-2").toggleClass('change-ul-color');
+		$(".h6-2 span").text("-");
+	});
+	
+	$(".h6-3").click(function(){
+		
+		$('.ul-3').slideToggle('slow');
+		$(".h6-3").toggleClass('change-ul-color');
+		$(".h6-3 span").text("-");
+	 	
+	});
+	
+	$(".h6-4").click(function(){
+		
+		$('.ul-4').slideToggle('slow');
+		$(".h6-4").toggleClass('change-ul-color');
+		$(".h6-4 span").text("-");
+	});
+	
+})
